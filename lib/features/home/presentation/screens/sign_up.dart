6616f9220b5d97e4f4b43d1303ac80c19f6550b4
David Marshal <otonyemarshal123@gmail.com>
2023-05-25 17:16:25 +0100

    Finished Sign Up Page

--- lib/features/home/presentation/screens/sign_up.dart
+++ lib/features/home/presentation/screens/sign_up.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 
 class SignUp extends StatefulWidget {
@@ -45,7 +46,7 @@ class _SignUpState extends State<SignUp> {
             ),
           ),
           SizedBox(
-            height: 100,
+            height: 70,
           ),
           Padding(
             padding: const EdgeInsets.only(left: 50),
@@ -55,41 +56,152 @@ class _SignUpState extends State<SignUp> {
                 fontSize: 35)),
           ),
           SizedBox(
-            height: 60,
+            height: 30,
           ),
           Container(
-            height: 522,
+            height: 582,
             width: 500,
             decoration: BoxDecoration(
-              color: Colors.green,
+              color: Colors.white,
               borderRadius: BorderRadius.only(topRight: Radius.circular(40), topLeft: Radius.circular(40))
             ),
             child: Column(
               children: [
                 Padding(
-                  padding: const EdgeInsets.only(left: 50, top: 50),
+                  padding: const EdgeInsets.only(left: 50, top: 40),
                   child: Row(
                     children: [
-                      Text("New\nAccount", style: TextStyle(fontSize: 40),),
+                      Text("New\nAccount", style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: Colors.cyan[900] ),),
                       Padding(
                         padding: const EdgeInsets.only(left: 80, bottom: 20),
                         child: Column(
                           children: [
                             CircleAvatar(
                               radius: 40,
-                              backgroundColor: Colors.white,
+                              backgroundColor: Colors.cyan[900],
+                              child: CircleAvatar(
+                                backgroundColor: Colors.white,
+                                  radius: 38,
+                               child: Icon(Icons.camera_alt_outlined, size: 40, color: Colors.black,),
+                              ),
                             ),
                             SizedBox(
-                             height: 10,
+                             height: 4,
                             ),
-                            Text("Upload Picture")
+                            Text("Upload Picture",)
                           ],
                         ),
-                      )
+                      ),
+
                     ],
                   ),
-                )
+                ),
+                Align(
+                  alignment: Alignment.topLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 40),
+                      child: Text("Email", style: TextStyle(color: Colors.grey, fontSize: 18),),
+                    )
+                ),
+                SizedBox(
+                  height: 2,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 30, right: 30),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: "skiplabtech@gmail.com",
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Colors.cyan[900]!,
+                          width: 2!
+                        )
+                      ),
+                      prefixIcon: Icon(Icons.email,
+                      color: Colors.black,
 
+                      )
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Align(
+                    alignment: Alignment.topLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 40),
+                      child: Text("Username", style: TextStyle(color: Colors.grey, fontSize: 18),),
+                    )
+                ),
+                SizedBox(
+                  height: 2,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 30, right: 30),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: "Skiplab",
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Colors.cyan[900]!,
+                                width: 2!
+                            )
+                        ),
+                        prefixIcon: SvgPicture.asset("assets/icons/user.svg",
+                            height: 25,
+                          width: 25,
+                          fit: BoxFit.scaleDown,
+                        )
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Align(
+                    alignment: Alignment.topLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 40),
+                      child: Text("Password", style: TextStyle(color: Colors.grey, fontSize: 18),),
+                    )
+                ),
+                SizedBox(
+                  height: 2,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 30, right: 30),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: "******",
+                      enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.cyan[900]!,
+                              width: 2!
+                          )
+                      ),
+                        prefixIcon: Icon(Icons.key,
+                        color: Colors.black,
+                          size: 30,
+                        ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 95,
+                  width: 330,
+                  decoration: BoxDecoration(
+                      color: Colors.cyan[900],
+                      borderRadius: BorderRadius.circular(50)
+                  ),
+                  child: Align(
+                      alignment: Alignment.center,
+                      child: Text("Sign up", style: TextStyle(fontSize: 25, color: Colors.white),)
+                  ),
+                )
               ],
             ),
           )
